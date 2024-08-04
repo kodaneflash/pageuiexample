5ddffa4f786adbd7ecaef79fe4174ba6aeb68be4
kodaneflash <jamesonmarcher@gmail.com>
2024-08-04 15:47:04 -0400

    e

--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,7 @@ export default function Component() {
     },
   ];
 
-  const numberOfPortfolios = 0; // Replace with actual number or fetch from API
+  const numberOfPortfolios = 1500; // Replace with actual number or fetch from API
 
   return (
     <>
